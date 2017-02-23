37e11c6a7dffb00a5693dd2b68db8a7360f0a6fe
Eric Kanarr <erickanarr@gmail.com>
2017-02-23 09:27:30 -0500

    Delete triangle.py

--- triangle.py
+++
@@ -1,9 +0,0 @@
-# print a triangle made of stars, with a base of 5
-def count_up(start, end):
-    if start <= end:
-        print(start)
-        count_up(start + 1, end)
-    return
-
-
-count_up(0, 10)
